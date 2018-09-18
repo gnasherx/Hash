a73a6bbf46f794fd656915ca9c3bf4f22a26ce2c
gnasherx <pawarg124@gmail.com>
2018-09-18 23:27:08 +0530

    change 1

--- script.js
+++ script.js
@@ -27,29 +27,6 @@ document.getElementById('submit').onclick = function (e) {
 };
 
 
-document.getElementById('signin_submit').onclick = function (e) {
-    e.preventDefault();
 
-    // gets data from input text
-    var signin_email = document.getElementById('signin_email').value;
-    var signin_password = document.getElementById('signin_password').value;
-
-    console.log(signin_email, signin_password)
-
-    fetch('http://localhost:8000/api/login', {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: signin_email,
-            password: signin_password
-        })
-    }).then(res=>res.json())
-        .then(res => console.log(res));
-
-
-};
 
 
