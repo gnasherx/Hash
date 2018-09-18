d2d6f1588bb5f4bc7dd98db6dbe572966a00a2ef
gnasherx <pawarg124@gmail.com>
2018-09-18 23:16:02 +0530

    small changes in index.html

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 document.getElementById('submit').onclick = function (e) {
     // gets data from input text
     var name = document.getElementById('name').value;
@@ -7,7 +6,6 @@ document.getElementById('submit').onclick = function (e) {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
-    console.log(name, username, email, password );
 
     fetch('http://localhost:8000/api/signup', {
         method: 'post',
@@ -27,4 +25,31 @@ document.getElementById('submit').onclick = function (e) {
 
     e.preventDefault();
 };
+
+
+document.getElementById('signin_submit').onclick = function (e) {
+    e.preventDefault();
+
+    // gets data from input text
+    var signin_email = document.getElementById('signin_email').value;
+    var signin_password = document.getElementById('signin_password').value;
+
+    console.log(signin_email, signin_password)
+
+    // fetch('http://localhost:8000/api/login', {
+    //     method: 'post',
+    //     headers: {
+    //         'Accept': 'application/json, text/plain, */*',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         email: signin_email,
+    //         password: signin_password
+    //     })
+    // }).then(res=>res.json())
+    //     .then(res => console.log(res));
+
+
+};
+
 
